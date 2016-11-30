3716924e6cc71319f48e195d8e18691f8317db4b
Daniel Simmonds <simmondsdt@gmail.com>
2016-11-30 13:42:09 -0700

    user list

--- app/assets/javascripts/jquery_ex.js
+++ app/assets/javascripts/jquery_ex.js
@@ -1,13 +1,46 @@
 $(document).ready(function () {
+  if(window.location.pathname === '/userlist') {
 
-  function getName() {
-    var name = prompt('What is your name?').trim();
-    if (name ===  null || name === '') {
-      getName();
-    } else {
-      $('#main-header').text('Welcome ' + name);
+    function getName() {
+      var name = prompt('What is your name?');
+      if (name ===  null || name === '') {
+        getName();
+      } else {
+        $('#main-header').text('Welcome ' + name);
+      }
     }
-  }
 
-  getName();
+    var $userInput = $('#user-input');
+    var $userAdd = $('#add-user');
+    var $userList = $('#users-list');
+    var $toggleUserList = $('#toggle-user-list');
+
+    $toggleUserList.click(function () {
+      $userList.slideToggle();
+    });
+
+    function addUser () {
+      $userInput.removeClass('red-border');
+      var userInputValue = $userInput.val().trim();
+      if(userInputValue === '') {
+        alert('You need to input a user name!');
+        $userInput.addClass('red-border').focus();
+      } else {
+        $userList.append('<li>' + userInputValue + '</li>')
+        $userInput.val('').focus();
+      }
+    }
+
+    $userInput.keyup(function(e) {
+      if(e.keyCode === 13) {
+        addUser();
+      }
+    });
+
+    $userAdd.click(function() {
+      addUser();
+    });
+
+    getName();
+  }
 });
